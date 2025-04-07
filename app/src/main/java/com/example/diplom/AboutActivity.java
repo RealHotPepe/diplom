ad3db040d5156926a636cd5d2bfcd506ca4d2a8e
ReallPepe <slava@yandex.ru>
2025-04-07 12:32:05 +0400

    <fix> fixing some methods and design

--- app/src/main/java/com/example/diplom/AboutActivity.java
+++ app/src/main/java/com/example/diplom/AboutActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -15,14 +16,15 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         TextView textAbout = findViewById(R.id.textAbout);
-
         textAbout.setText(R.string.textAbout);
+
+        Button back = findViewById(R.id.back);
+        back.setOnClickListener(v -> goBack());
 
     }
 
-    public void goBack (View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    private void goBack (){
+        startActivity(new Intent(this, MainActivity.class));
     }
 
 }
--- app/src/main/java/com/example/diplom/MainActivity.java
+++ app/src/main/java/com/example/diplom/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button about = findViewById(R.id.about);
+        Button testing = findViewById(R.id.testing);
+
+        about.setOnClickListener(v -> goAbout());
+        testing.setOnClickListener(v -> goTesting());
+
     }
 
-    public void goAbout (View view){
-        Intent intent = new Intent(this, AboutActivity.class);
-        startActivity(intent);
+    private void goAbout (){
+        startActivity(new Intent(this, AboutActivity.class));
     }
-    public void goTesting (View view){
-        Intent intent = new Intent(this, TestingActivity.class);
-        startActivity(intent);
+    private void goTesting (){
+        startActivity(new Intent(this, TestingActivity.class));
     }
 }
